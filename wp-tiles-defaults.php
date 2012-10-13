68e3feabc5027bf3e127385ce1ee2ae1dfc57b1d
mike <mike@TrenvOS>
2012-10-13 12:47:34 +0200

    Defaults

---
+++ wp-tiles-defaults.php
@@ -0,0 +1,59 @@
+<?php
+global $wptiles_defaults;
+$wptiles_defaults = array(
+                // Post query
+                "posts_query"         => array (
+                                    'numberposts'   => 20,
+                                    'offset'        => 0,
+                                    'category'      => 0,
+                                    'orderby'       => 'rand',
+                                    'order'         => 'DESC',
+                                    'include'       => array(),
+                                    'exclude'       => array(),
+                                    'meta_key'      => '',
+                                    'meta_value'    =>'',
+                                    'post_type'     => 'post',
+                                    'suppress_filters'
+                                                    => true,
+                ),
+                'colors'        => array (
+                    "colors"    =>
+                                  "#009999\n"
+                                . "#1D7373\n"
+                                . "#006363\n"
+                                . "#33CCCC\n"
+                                . "#5CCCCC"
+                ),
+                'templates'     => array (
+                    "templates"     => array (
+                                            " . A A B B \n"
+                                          . " C C . B B \n"
+                                          . " D D E E . \n"
+                                          . " D D . C C \n",
+
+                                            " . A A D B \n"
+                                          . " C A A D B \n"
+                                          . " G F E E . \n"
+                                          . " G F . C C \n",
+
+                                            " A B C C D \n"
+                                          . " A B C C D \n"
+                                          . " G F E E . \n"
+                                          . " G F . H H \n",
+
+                                          " . . . . . ",
+
+                                          " . . . . . . . . ",
+
+                                    ),
+                    "small_screen_template" =>
+                                          " A A \n"
+                                        . " . . \n"
+                                        . " A A \n"
+                                        . " . . \n"
+                                        . " A A \n"
+                                        . " . . \n"
+                                        . " A A "
+                    ,
+                ),
+            );
