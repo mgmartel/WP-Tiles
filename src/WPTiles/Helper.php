14c4f2ef7dc74e7d08d2edc24f4bf0b3722b5c9a
Mike Martel <mike@trenvo.com>
2014-04-30 16:59:33 +0200

    Added Helper::colors_to_rgba

--- src/WPTiles/Helper.php
+++ src/WPTiles/Helper.php
@@ -6,9 +6,23 @@ if ( !defined ( 'ABSPATH' ) )
 
 class Helper
 {
+    public static function colors_to_rgba( $colors ) {
+        $rgba = array();
+        foreach( $colors as $c ) {
+            if ( strpos( $c, 'rgba' ) === 0 )
+                $rgba[] = $c;
+            if ( strpos( $c, 'rgb' ) === 0 )
+                $rgba[] = Helper::rgb_to_rgba( $c, 1, true );
+            elseif( strpos( $c, '#' ) === 0 )
+                $rgba[] = Helper::hex_to_rgba( $c, 1, true );
+        }
+
+        return $rgba;
+    }
+
     public static function hex_to_rgb( $hex ) {
         $hex   = str_replace( "#", "", $hex );
-        $color = array( );
+        $color = array( 'r' => 0, 'g' => 0, 'b' => 0 );
 
         if ( strlen( $hex ) == 3 ) {
             $color['r'] = hexdec( substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) );
@@ -23,9 +37,10 @@ class Helper
         return $color;
     }
 
-    public static function rgba_to_rgba( $rgb, $alpha = 1, $css = false ) {
-        $rgba = array( 'r' => '', 'g' => '', 'b' => '' );
-        preg_match_all( '[0-9]{3}', $rgb, $rgba );
+    public static function rgb_to_rgba( $rgb, $alpha = 1, $css = false ) {
+        $matches = array();
+        preg_match_all( '/[0-9]{1,3}/', $rgb, $matches );
+        $rgba = array_combine( array( 'r', 'g', 'b' ), array_slice( reset( $matches ), 0, 3 ) );
 
         $rgba['a'] = $alpha;
         if ( !$css )
