1913baecb1ffd54e50bb94d76abeed4f9f12ec3b
Mike Martel <mike@trenvo.com>
2014-05-06 12:07:25 +0200

    Added 'build' script (minifies everything and updates classmap)

--- config.rb
+++ config.rb
@@ -9,9 +9,10 @@ javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :nested
+output_style = :expanded
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
