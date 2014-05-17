d6003366d488560cf973dae76581776d0cb7513c
Mike Martel <mike@trenvo.com>
2014-05-17 12:51:20 +0200

    Add admin notice if user tries to activate WP Tiles on PHP<5.3

--- wp-tiles.php
+++ wp-tiles.php
@@ -12,66 +12,76 @@
 if ( !defined( 'ABSPATH' ) )
     exit;
 
-/**
- * Version number
- *
- * @since 0.1
- */
-define( 'WP_TILES_VERSION', '1.0-beta1' );
+if(version_compare(PHP_VERSION, '5.3', '<')) {
+    function wp_tiles_php53_dashboard_notice() {
+        echo __('<div class="error"><p>WP Tiles is <strong>not</strong> active. This version of the plugin requires PHP v5.3+.</p></div>', 'wp-tiles');
+    }
+    add_action( 'all_admin_notices', 'wp_tiles_php53_dashboard_notice' );
 
-/**
- * PATHs and URLs
- *
- * @since 0.1
- */
+} else {
 
-define( 'WP_TILES_DIR', plugin_dir_path( __FILE__ ) );
-define( 'WP_TILES_URL', plugin_dir_url( __FILE__ ) );
-define( 'WP_TILES_TEMPLATES_DIR', WP_TILES_DIR . 'templates/' );
-define( 'WP_TILES_TEMPLATES_URL', WP_TILES_URL . 'templates/' );
-define( 'WP_TILES_ASSETS_URL', WP_TILES_URL . 'assets/' );
+    /**
+     * Version number
+     *
+     * @since 0.1
+     */
+    define( 'WP_TILES_VERSION', '1.0-beta1' );
 
-/**
- * Requires and includes
- *
- * @since 1.0
- */
-if ( !defined( 'VP_VERSION' ) )
-    require plugin_dir_path( __FILE__ ) .'vafpress-framework/bootstrap.php';
+    /**
+     * PATHs and URLs
+     *
+     * @since 0.1
+     */
 
-require WP_TILES_DIR . 'vendor/autoload.php';
+    define( 'WP_TILES_DIR', plugin_dir_path( __FILE__ ) );
+    define( 'WP_TILES_URL', plugin_dir_url( __FILE__ ) );
+    define( 'WP_TILES_TEMPLATES_DIR', WP_TILES_DIR . 'templates/' );
+    define( 'WP_TILES_TEMPLATES_URL', WP_TILES_URL . 'templates/' );
+    define( 'WP_TILES_ASSETS_URL', WP_TILES_URL . 'assets/' );
 
-register_activation_hook( __FILE__, array( 'WPTiles\WPTiles', 'on_plugin_activation' ) );
+    /**
+     * Requires and includes
+     *
+     * @since 1.0
+     */
+    if ( !defined( 'VP_VERSION' ) )
+        require plugin_dir_path( __FILE__ ) .'vafpress-framework/bootstrap.php';
 
-if ( get_option( 'wp-tiles-options' ) ) {
-    WPTiles\Legacy::convert_option();
+    require WP_TILES_DIR . 'vendor/autoload.php';
+
+    register_activation_hook( __FILE__, array( 'WPTiles\WPTiles', 'on_plugin_activation' ) );
+
+    if ( get_option( 'wp-tiles-options' ) ) {
+        WPTiles\Legacy::convert_option();
+    }
+
+    /**
+     * Get the one and only true instance of WP Tiles
+     *
+     * @return WPTiles\WPTiles
+     * @since 0.4.2
+     */
+    function wp_tiles() {
+        return \WPTiles\WPTiles::get_instance();
+    }
+
+    // Initialize
+    wp_tiles();
+
+    add_action( 'plugins_loaded', 'wptiles_load_pluggables' );
+    function wptiles_load_pluggables() {
+        require_once( WP_TILES_DIR . '/wp-tiles-pluggables.php' );
+    }
+
+    function wp_tiles_preview_tile() {
+        return WPTiles\Admin\Admin::preview_tile();
+    }
+
+    // Add settings link
+    $plugin = plugin_basename( __FILE__ );
+    add_filter( "plugin_action_links_$plugin", function( $links ){
+        $links[] = '<a href="admin.php?page=wp-tiles">' . __( 'Settings', 'wp-tiles' ) . '</a>';
+        return $links;
+    } );
+
 }
-
-/**
- * Get the one and only true instance of WP Tiles
- *
- * @return WPTiles\WPTiles
- * @since 0.4.2
- */
-function wp_tiles() {
-    return \WPTiles\WPTiles::get_instance();
-}
-
-// Initialize
-wp_tiles();
-
-add_action( 'plugins_loaded', 'wptiles_load_pluggables' );
-function wptiles_load_pluggables() {
-    require_once( WP_TILES_DIR . '/wp-tiles-pluggables.php' );
-}
-
-function wp_tiles_preview_tile() {
-    return WPTiles\Admin\Admin::preview_tile();
-}
-
-// Add settings link
-$plugin = plugin_basename( __FILE__ );
-add_filter( "plugin_action_links_$plugin", function( $links ){
-    $links[] = '<a href="admin.php?page=wp-tiles">' . __( 'Settings', 'wp-tiles' ) . '</a>';
-    return $links;
-} );
