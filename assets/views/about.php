0607c45cc923171ba8b21a0c7a26e7fc0f37a54c
Mike Martel <mike@trenvo.com>
2015-01-26 15:58:23 +0700

    Added about box to admin

---
+++ assets/views/about.php
@@ -0,0 +1,22 @@
+<div class='wp-tiles-stuffbox stuffbox' id="about-wp-tiles">
+
+    <h3>About WP Tiles</h3>
+
+    <div class='inside'>
+        
+        <p>Hi! Thank you for using our plugin WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
+            If you need support, you can head over to the <a href='https://wordpress.org/support/plugin/wp-tiles' target='_blank'>support forums</a> on WordPress.org.</p>
+
+        <p>If you enjoy the support we provide on this free plugin, please take moment to check out our WordPress maintenance offering <a href='https://remaintenance.io' target='_blank'>Remaintenance</a>.</p>
+        <p>With Remaintenance, we will pro-actively monitor your website for performance, uptime and SEO and let you know if something is wrong. We will also take care of all plugin, theme and core updates and always ensure that everything is still working afterwards.</p>
+        <p>Interested? Head over to <a href='https://remaintenance.io' target='_blank'>Remaintenance.io</a> to read more!</p>
+
+        <p>-- Mike</p>
+
+        <a href='https://wordpress.org/support/profile/mike_cowobo' target='_blank'>
+            <img alt='Mike' src='http://1.gravatar.com/avatar/b2e3ffa705cde285c2fb9ce596c85126?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G' class='avatar avatar-96 photo' height='96' width='96' />
+        </a>
+
+    </div>
+
+</div>
