4bb951806e4b1dac711192b1f6f7521c71df5874
Mike Martel <mike@trenvo.com>
2015-02-02 16:35:25 +0700

    Updated the about box with mailchimp subscribe form

--- assets/views/about.php
+++ assets/views/about.php
@@ -3,19 +3,42 @@
     <h3>About WP Tiles</h3>
 
     <div class='inside'>
-        
-        <p>Hi! Thank you for using our plugin WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
+
+        <p>Thank you for using our plugin WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
             If you need support, you can head over to the <a href='https://wordpress.org/support/plugin/wp-tiles' target='_blank'>support forums</a> on WordPress.org.</p>
 
-        <p>If you enjoy the support we provide on this free plugin, please take moment to check out our WordPress maintenance offering <a href='https://remaintenance.io' target='_blank'>Remaintenance</a>.</p>
-        <p>With Remaintenance, we will pro-actively monitor your website for performance, uptime and SEO and let you know if something is wrong. We will also take care of all plugin, theme and core updates and always ensure that everything is still working afterwards.</p>
-        <p>Interested? Head over to <a href='https://remaintenance.io' target='_blank'>Remaintenance.io</a> to read more!</p>
+        <p>If you enjoy the support we provide on this free plugin, please take moment to <strong>check out our WordPress maintenance offering <a href='https://remaintenance.io' target='_blank'>Remaintenance</a></strong>.</p>
+        <p>With Remaintenance, we will pro-actively monitor your website for performance, uptime and SEO and let you know if something is wrong. We will also take care of all plugin, theme and core updates and always ensure that everything is still working.</p>
+        <p>On top of that is 4 hours of dedicated support time <em>each month</em>. Interested? Head over to <a href='https://remaintenance.io' target='_blank'>Remaintenance.io</a> to read more!</p>
 
         <p>-- Mike</p>
 
         <a href='https://wordpress.org/support/profile/mike_cowobo' target='_blank'>
             <img alt='Mike' src='http://1.gravatar.com/avatar/b2e3ffa705cde285c2fb9ce596c85126?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G' class='avatar avatar-96 photo' height='96' width='96' />
-        </a>
+        </a><br />
+
+        <hr>
+
+        <p><strong>Stay up to date</strong><br />
+        To stay up to date with the development of WP Tiles, please sign up to our newsletter below:</p>
+
+        <!-- Begin MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+            <form action="http://trenvo.us2.list-manage.com/subscribe/post?u=999667a0c77440ad37655daa6&amp;id=7b9dc606b6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div class="mc-field-group">
+                    <input type="email" value="<?php echo wp_get_current_user()->user_email ?>" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+                    <input type="hidden" value="<?php site_url() ?>" name="SITE" class=" url" id="mce-SITE"><br /><br />
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button-primary">
+                </div>
+
+                <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;"><input type="text" name="b_999667a0c77440ad37655daa6_7b9dc606b6" tabindex="-1" value=""></div>
+            </form>
+        </div>
+        <!--End mc_embed_signup-->
 
     </div>
 
