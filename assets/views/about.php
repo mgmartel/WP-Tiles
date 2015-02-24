4fbd015f5a4812e4f74f3a0981ed4a10b2c6c4fd
Mike Martel <mike@trenvo.com>
2015-02-24 11:37:21 +0700

    Updated the about box

--- assets/views/about.php
+++ assets/views/about.php
@@ -4,12 +4,8 @@
 
     <div class='inside'>
 
-        <p>Thank you for using our plugin WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
+        <p>Thank you for using WP Tiles. If you are ever stuck using the plugin you can check out the detailed documentation and example pages on <a href='http://wp-tiles.com/' target='_blank'>wp-tiles.com</a>.
             If you need support, you can head over to the <a href='https://wordpress.org/support/plugin/wp-tiles' target='_blank'>support forums</a> on WordPress.org.</p>
-
-        <p>If you enjoy the support we provide on this free plugin, please take moment to <strong>check out our WordPress maintenance offering <a href='https://remaintenance.io' target='_blank'>Remaintenance</a></strong>.</p>
-        <p>With Remaintenance, we will pro-actively monitor your website for performance, uptime and SEO and let you know if something is wrong. We will also take care of all plugin, theme and core updates and always ensure that everything is still working.</p>
-        <p>On top of that is 4 hours of dedicated support time <em>each month</em>. Interested? Head over to <a href='https://remaintenance.io' target='_blank'>Remaintenance.io</a> to read more!</p>
 
         <p>-- Mike</p>
 
@@ -17,9 +13,43 @@
             <img alt='Mike' src='http://1.gravatar.com/avatar/b2e3ffa705cde285c2fb9ce596c85126?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G' class='avatar avatar-96 photo' height='96' width='96' />
         </a><br />
 
-        <hr>
+    </div>
 
-        <p><strong>Stay up to date</strong><br />
+    <hr />
+
+    <h3>Become a Remaintenance Affiliate</h3>
+
+    <div class='inside'>
+
+        <p>Our (awesome) WordPress maintenance service is now offering an affiliate program with <strong>massive pay-outs</strong>.</p>
+
+        <p>For every referral, you get <strong>€150</strong>. And if your referral pays a year in advance, we'll double it for a total of <strong>€300</strong>. Easy money!</p>
+
+        <p>Read how you can become an affiliate on <a href='https://remaintenance.io/affiliate-program/' target='_blank'>our website</a>.</p>
+
+
+    </div>
+
+    <hr />
+
+    <h3>TiledFourteen</h3>
+    <span class='subtitle'>Our free WP Tiles theme</span>
+
+    <div class='inside'>
+
+        <p>Did you know that we also have a free theme especially for WP Tiles based websites?
+        It's a child theme of Twenty Fourteen.
+        You can see it in action on the demo pages on <a href='http://wp-tiles.com' target='_blank'>our website</a>.
+        Check out the theme page <a href='http://wp-tiles.com/tiledfourteen/' target='_blank'>here</a>!</p>
+
+    </div>
+
+    <hr />
+
+    <h3>Stay up to date</h3>
+
+    <div class='inside'>
+
         To stay up to date with the development of WP Tiles, please sign up to our newsletter below:</p>
 
         <!-- Begin MailChimp Signup Form -->
